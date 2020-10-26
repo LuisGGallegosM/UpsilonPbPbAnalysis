9933aae04c6e4fc1bed0ab733890b4cea0268384
Gabriel <luisg.gallegosm@outlook.com>
2020-10-26 11:44:40 -0700

    namespace using.

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,16 @@
 
 #include "Main.h"
 
+using namespace std;
+
 void Main()
 {
-    std::string data_filename ="files/merged_HiForestAOD.root";
-    std::string skimmed_filename = "files/merged_HiForestAOD_skimmed.root";
-    std::string folder_name   = "files/fit_exlimit";
+    string data_filename ="files/merged_HiForestAOD.root";
+    string skimmed_filename = "files/merged_HiForestAOD_skimmed.root";
+    string folder_name   = "files/fit_exlimit";
 
-    std::string fit_filename = folder_name + "/oniafit.root";
-    std::string drawing_filename = folder_name + "/oniafit.pdf";
+    string fit_filename = folder_name + "/oniafit.root";
+    string drawing_filename = folder_name + "/oniafit.pdf";
   
     kineCutParam kineCut;
     kineCut.ptLow=0.0f;
@@ -19,7 +21,7 @@ void Main()
     kineCut.massHigh =10.0f;
     kineCut.singleMuPtLow=3.5f;
     kineCut.singleMuEtaHigh=2.4f;
-    kineCut.nBins = (kineCut.massHigh- kineCut.massLow)*100;
+    kineCut.nBins = (kineCut.massHigh- kineCut.massLow)*800;
     kineCut.bkgOn =true;
 
     gSystem->mkdir(folder_name.data());
@@ -28,7 +30,7 @@ void Main()
     //Skim(data_filename.data(),skimmed_filename.data());
 
     //Mass Fitting function
-    massfit(skimmed_filename.data(),fit_filename.data(),&kineCut);
+    //massfit(skimmed_filename.data(),fit_filename.data(),&kineCut);
 
     //Generate drawings
     Drawing(fit_filename.data(),drawing_filename.data(),&kineCut);
