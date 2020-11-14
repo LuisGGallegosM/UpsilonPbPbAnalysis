5ce5dc73a579f56ebdfeb80d7dd05719c4a3a1b6
Gabriel <luisg.gallegosm@outlook.com>
2020-11-14 16:58:40 -0700

    Added fitConfGen

---
+++ fitconfGen.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+OUTPUT="bkgOn = true\n"
+OUTPUT+="moreUpsilon = false\n"
+OUTPUT+="massHigh = 10.0\n"
+OUTPUT+="massLow = 8.0\n"
+OUTPUT+="cut.ptLow = %s\n"
+OUTPUT+="cut.ptHigh = %s\n"
+OUTPUT+="cut.yLow = 0.0\n"
+OUTPUT+="cut.yHigh = 2.4\n"
+OUTPUT+="initialValues.nSigY1S = 457044\n"
+OUTPUT+="initialValues.nSigY2S = 457044\n"
+OUTPUT+="initialValues.nSigY3S = 457044\n"
+OUTPUT+="initialValues.nBkg = 23153.3\n"
+OUTPUT+="initialValues.chk4_k1 = -0.35\n"
+OUTPUT+="initialValues.chk4_k2 = -0.62\n"
+OUTPUT+="initialValues.dcb.mean = 9.46\n"
+OUTPUT+="initialValues.dcb.alpha = 1.4\n"
+OUTPUT+="initialValues.dcb.n = 3.0\n"
+OUTPUT+="initialValues.dcb.sigma1 = 0.1\n"
+OUTPUT+="initialValues.dcb.x = 0.55\n"
+OUTPUT+="initialValues.dcb.f = 0.5\n"
+
+OUTDIR="../rootfiles/merged_HiForestAOD_MC_skimmed"
+BASENAME="merged_HiForestAOD_fit"
+
+pt=( 0.0 2.0 4.0 6.0 9.0  12.0 30.0 )
+
+num=$((${#pt[@]} -2 ))
+ 
+for i in $(seq 0 $num )
+do
+ printf "$OUTPUT" "${pt[$i]}" "${pt[ $(($i+1)) ]}" > "${OUTDIR}/${BASENAME}${i}.fitconf"
+done
