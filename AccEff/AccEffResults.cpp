7b17c1a11c8dab8c1dc0e6195ec0bd256fb63879
Gabriel <luisg.gallegosm@outlook.com>
2021-03-05 21:59:50 -0700

    expansion of AccEffResults.

--- AccEff/AccEffResults.cpp
+++ AccEff/AccEffResults.cpp
@@ -8,7 +8,7 @@
 #include "AccAnalyzer.h"
 #include "EffAnalyzer.h"
 
-void AccEffResults(const char* accFilename, const char* effFilename, const char* fitfilepath, const char* outputname)
+void AccEffResults(const char* accFilename, const char* effFilename, const char* fitFilepath, const char* outputname)
 {
     std::cout << "\nACCEPTANCY EFFICIENCY RESULT TEST\n";
 
@@ -19,8 +19,8 @@ void AccEffResults(const char* accFilename, const char* effFilename, const char*
     std::cout << "Reading effiency input file: " << effFilename <<'\n';
     TFile* effFile = OpenFile(effFilename,"READ");
     //input file
-    std::cout << "Reading multifit file: " << fitfilepath <<'\n';
-    TFile* fitFile = OpenFile(fitfilepath,"READ");
+    std::cout << "Reading multifit file: " << fitFilepath <<'\n';
+    TFile* fitFile = OpenFile(fitFilepath,"READ");
     //reading file
     std::cout << "Writing to file: " << outputname <<'\n';
     TFile* outFile = OpenFile(outputname,"CREATE");
@@ -30,14 +30,23 @@ void AccEffResults(const char* accFilename, const char* effFilename, const char*
     TH1F* acc_den= (TH1F*) accFile->Get(accDenName);
     TH1F* eff_den= (TH1F*) effFile->Get(effDenName);
 
-    TH1F dens= (*acc_den)*(*eff_den);
-    TH1F nums= (*acc_num)*(*eff_num);
+    TH1F* dens= new TH1F((*acc_den)*(*eff_den));
+    TH1F* nums= new TH1F((*acc_num)*(*eff_num));
 
-    std::unique_ptr<TEfficiency> AccXEff = createTEff(&nums,&dens,"pt QQ AccXEff");
+    std::unique_ptr<TEfficiency> AccXEff = createTEff(nums,dens,"pt QQ AccXEff");
 
-    std::string outbasename= ReplaceExtension(outputname,"Final.pdf");
+    std::string outbasename= ReplaceExtension(outputname,"");
 
-    writeToCanvasEff(AccXEff.get(),"p_{T}","Acc x Eff",outbasename);
+    writeToCanvasEff(AccXEff.get(),"p_{T}","Acc x Eff",outbasename+"_AccXEff.pdf");
+    
+    TH1F* nSig = (TH1F*) fitFile->Get("nSigY1S");
+
+    TH1F* nSigCorrectedNum = new TH1F((*nSig)*(*dens) );
+    TH1F* nSigCorrectedDen = new TH1F((*nums));
+
+    std::unique_ptr<TEfficiency> nSigCorrected = createTEff(nSigCorrectedNum,nSigCorrectedDen,"corrected raw yields");
+
+    writeToCanvasEff(nSigCorrected.get(),"p_{T}","Corrected raw yields",outbasename+"_CorrectYields.pdf");
 
     AccXEff->Write();
 
