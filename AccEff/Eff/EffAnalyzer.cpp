f58ee09db8c3377576bd588e34ce57c0e1167445
Gabriel <luisg.gallegosm@outlook.com>
2021-03-31 19:36:49 -0700

    extracted class with histograms from EffAnalyzer

--- AccEff/Eff/EffAnalyzer.cpp
+++ AccEff/Eff/EffAnalyzer.cpp
@@ -8,12 +8,16 @@
 
 #include "../Common/AccEffAux.h"
 
-template<typename Reader>
-EffAnalyzerBase<Reader>::EffAnalyzerBase(TTree* input,CutParams* effCut, const char* outTreeName ) : 
-    TreeProcessor(input,outTreeName),oniaReader(),oniaWriter(),effCutter(effCut)
+std::unique_ptr<EffAnalyzer> createEffAnalyzer(TTree* input,CutParams* effCut, const char* outTreeName )
 {
-    registerInputs(&oniaReader);
-    registerOutputs(&oniaWriter);
+    if (effCut->getIsMC())
+        return std::unique_ptr<EffAnalyzer>(new EffAnalyzerMC(input,effCut,outTreeName));
+    else
+        return nullptr;
+}
+
+EffHistografer::EffHistografer()
+{
     //initialize Histograms
     etaVsPtQQRecoCut =  createTH2QQ("y vs pt QQ Reco + Cut",    "p^{#mu#mu}_{t} vs |y^{#mu#mu}| QQ Reco + Cut");
     etaVsPtMuRecoCut =  createTH2Mu("eta vs pt Mu Reco + Cut",  "p^{#mu}_{t} vs |#eta^{#mu}| Mu Reco + Cut");
@@ -23,22 +27,8 @@ EffAnalyzerBase<Reader>::EffAnalyzerBase(TTree* input,CutParams* effCut, const c
     ptHistQQDet=        createTH1(effDenName,"p^{#mu#mu}_{t} QQ Detectable");
 }
 
-template<typename Reader>
-void EffAnalyzerBase<Reader>::ProcessEvent(Long64_t entry)
+void EffHistografer::Write(const std::string& basename)
 {
-    int size=oniaReader.getData()->genQQ.size;
-    
-    for(int i=0;i<size;++i)
-    {
-        Analyze(i,entry);
-    }
-}
-
-template<typename Reader>
-void EffAnalyzerBase<Reader>::Write(const std::string& basename)
-{
-    TreeProcessor::write();
-
     //write 2D plots
     writeToCanvas(etaVsPtQQRecoCut,"|y^{#mu#mu}|","p^{#mu#mu}_{T} ( GeV/c )",basename+"_EtaPtQQ_RecoCut.pdf");
     writeToCanvas(etaVsPtQQDet,"|y^{#mu#mu}|","p^{#mu#mu}_{T} ( GeV/c )",basename+"_EtaPtQQ_Det.pdf");
@@ -66,67 +56,16 @@ void EffAnalyzerBase<Reader>::Write(const std::string& basename)
     etaVsPtQQEfficiency->Write(0,TObject::kOverwrite);
 }
 
-template<typename Reader>
-void EffAnalyzerBase<Reader>::Analyze(Int_t index, Long64_t entry)
+void EffHistografer::FillDet(float y, float pT)
 {
-    auto input=oniaReader.getData();
-    if (accCutter->cut(input,index,entry))
-    {
-        CaptureDetQQ(index,entry);
-        int recoQQindex= input->which.GenQQWhichReco[index];
-        if (recoQQindex>=0)
-        {
-            if (effCutter.cut(input,recoQQindex,entry))
-                CaptureRecoQQ(recoQQindex,entry);
-        }
-    }
-}
-
-template<typename Reader>
-void EffAnalyzerBase<Reader>::CaptureDetQQ(Int_t index, Long64_t entry)
-{
-    TLorentzVector* mom4vec=(TLorentzVector*) oniaReader.getData()->genQQ.mom4->At(index);
-    float pT = mom4vec->Pt();
-    float y = fabs(mom4vec->Rapidity());
-
     etaVsPtQQDet->Fill(y,pT);
     ptHistQQDet->Fill(pT);
 }
 
-template<typename Reader>
-void EffAnalyzerBase<Reader>::CaptureRecoQQ(Int_t index, Long64_t entry)
+void EffHistografer::FillRecoCut(const inputs* input)
 {
-    auto input = oniaReader.getData();
-    //read variables
-    TLorentzVector* mom4vec=(TLorentzVector*) input->recoQQ.mom4->At(index);
-
-    int mupl_idx = input->recoQQ.mupl_idx[index];//plus muon index
-    int mumi_idx = input->recoQQ.mumi_idx[index];//minus muon index
-    TLorentzVector* mom4vecPl=(TLorentzVector*) input->recoMu.mom4->At(mupl_idx);
-    TLorentzVector* mom4vecMi=(TLorentzVector*) input->recoMu.mom4->At(mumi_idx);
-
-    float pT = mom4vec->Pt();
-    float y = fabs(mom4vec->Rapidity());
-
-    float etaMuPl=fabs(mom4vecPl->Eta());
-    float etaMuMi=fabs(mom4vecMi->Eta());
-    float ptMuPl=mom4vecPl->Pt();
-    float ptMuMi=mom4vecMi->Pt();
-
-    ptHistQQRecoCut->Fill(pT);
-    etaVsPtQQRecoCut->Fill(y,pT);
-    etaVsPtMuRecoCut->Fill(etaMuPl,ptMuPl);
-    etaVsPtMuRecoCut->Fill(etaMuMi,ptMuMi);
-    oniaWriter.writeData(input,SimpleSelector{entry,index});
-    FillEntries();
+    ptHistQQRecoCut->Fill(input->pT);
+    etaVsPtQQRecoCut->Fill(input->y,input->pT);
+    etaVsPtMuRecoCut->Fill(input->etaMuPl,input->ptMuPl);
+    etaVsPtMuRecoCut->Fill(input->etaMuMi,input->ptMuMi);
 }
-
-std::unique_ptr<EffAnalyzer> createEffAnalyzer(TTree* input,CutParams* effCut, const char* outTreeName )
-{
-    if (effCut->getIsMC())
-        return std::unique_ptr<EffAnalyzer>(new EffAnalyzerMC(input,effCut,outTreeName));
-    else
-        return nullptr;
-} 
-
-template class EffAnalyzerBase<OniaMCData>;
--- AccEff/Eff/EffAnalyzer.h
+++ AccEff/Eff/EffAnalyzer.h
@@ -17,10 +17,8 @@ class EffAnalyzer
     virtual ~EffAnalyzer() = default;
 };
 
-template<typename Reader>
-class EffAnalyzerBase : public TreeProcessor, public EffAnalyzer
+class EffHistografer
 {
-    private:
     TH2F* etaVsPtQQRecoCut;
     TH2F* etaVsPtMuRecoCut;
     TH2F* etaVsPtQQDet;
@@ -29,23 +27,107 @@ class EffAnalyzerBase : public TreeProcessor, public EffAnalyzer
     std::unique_ptr<TEfficiency> ptQQEfficiency;
     std::unique_ptr<TEfficiency> etaVsPtQQEfficiency;
 
+    public:
+    struct inputs
+    {
+        float pT;
+        float y;
+        float etaMuPl;
+        float etaMuMi;
+        float ptMuPl;
+        float ptMuMi;
+    };
+    EffHistografer();
+
+    void FillDet(float y, float pT);
+    void FillRecoCut(const inputs* input);
+    void Write(const std::string& basename);
+};
+
+template<typename Reader>
+class EffAnalyzerBase : public TreeProcessor, public EffAnalyzer
+{
+    EffHistografer hists;
     OniaReader<Reader> oniaReader;
     EffCutter effCutter;
     OniaWriterRecoQQ oniaWriter;
     std::unique_ptr<AccCutter> accCutter;
 
-    void Analyze(Int_t index, Long64_t entry);
-    void CaptureRecoQQ(Int_t index, Long64_t entry);
-    void CaptureDetQQ(Int_t index, Long64_t entry);
+    void CaptureRecoQQ(Int_t index, Long64_t entry)
+    {
+        auto input = oniaReader.getData();
+        EffHistografer::inputs data = extractData(input,index);
+
+        hists.FillRecoCut(&data);
+        oniaWriter.writeData(input,SimpleSelector{entry,index});
+        FillEntries();
+    }
+
+    void CaptureDetQQ(Int_t index, Long64_t entry)
+    {
+        TLorentzVector* mom4vec=(TLorentzVector*) oniaReader.getData()->genQQ.mom4->At(index);
+        float pT = mom4vec->Pt();
+        float y = fabs(mom4vec->Rapidity());
+        hists.FillDet(y,pT);
+    }
 
     public:
-    EffAnalyzerBase(TTree* input,CutParams* effCut, const char* outTreeName );
+    EffAnalyzerBase(TTree* input,CutParams* effCut, const char* outTreeName ): 
+        TreeProcessor(input,outTreeName),oniaReader(),oniaWriter(),effCutter(effCut)
+    {
+        registerInputs(&oniaReader);
+        registerOutputs(&oniaWriter);
+    }
 
-    void Write(const std::string& basename) override;
+    void Write(const std::string& basename) override
+    {
+        TreeProcessor::write();
+        hists.Write(basename);
+    }
 
     void Test() override { Process(); }
-    void ProcessEvent(Long64_t entry) override;
+
+    void ProcessEvent(Long64_t entry) override
+    {
+        int size=oniaReader.getData()->genQQ.size;
+        
+        for(int index=0;index<size;++index)
+        {
+            auto input=oniaReader.getData();
+            if (accCutter->cut(input,index,entry))
+            {
+                CaptureDetQQ(index,entry);
+                int recoQQindex= input->which.GenQQWhichReco[index];
+                if (recoQQindex>=0)
+                {
+                    if (effCutter.cut(input,recoQQindex,entry))
+                        CaptureRecoQQ(recoQQindex,entry);
+                }
+            }
+        }
+    }
 };
+
+template<typename Data>
+EffHistografer::inputs extractData( const Data* input ,int index)
+{
+    EffHistografer::inputs data;
+
+    TLorentzVector* mom4vec=(TLorentzVector*) input->recoQQ.mom4->At(index);
+
+    int mupl_idx = input->recoQQ.mupl_idx[index];//plus muon index
+    int mumi_idx = input->recoQQ.mumi_idx[index];//minus muon index
+    TLorentzVector* mom4vecPl=(TLorentzVector*) input->recoMu.mom4->At(mupl_idx);
+    TLorentzVector* mom4vecMi=(TLorentzVector*) input->recoMu.mom4->At(mumi_idx);
+
+    data.pT = mom4vec->Pt();
+    data.y = fabs(mom4vec->Rapidity());
+    data.etaMuPl=fabs(mom4vecPl->Eta());
+    data.etaMuMi=fabs(mom4vecMi->Eta());
+    data.ptMuPl=mom4vecPl->Pt();
+    data.ptMuMi=mom4vecMi->Pt();
+    return data;
+}
 
 using EffAnalyzerRealData = EffAnalyzerBase<OniaRealData>;
 using EffAnalyzerMC = EffAnalyzerBase<OniaMCData>;
